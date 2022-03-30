91403276c237e20f1fba26ab16fadecf66539ceb
unknown <lamica.olivares@gmail.com>
2022-03-30 12:43:52 -0300

    validaciones regex

--- src/component/Registro.jsx
+++ src/component/Registro.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
 import Helmet from 'react-helmet'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Registro = () => {
-  return (
+const [correo , establecerCorreo] = useState('');
+const [clave , establecerClave] = useState('');
+const [clave2 , establecerClave2] = useState('');
+
+const handleChange = (e) => {
+console.log(e.target.name)
+switch(e.target.name){
+  case 'email':
+    establecerCorreo(e.target.value);
+    break;
+    case 'password':
+    establecerClave(e.target.value);
+    break;
+    case 'password2':
+    establecerClave2(e.target.value);
+    break;
+    default:
+      break
+}
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log(correo, clave, clave2);
+
+  const expresionRegular = /[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+ if ( !expresionRegular.test(correo) ){
+    console.log('wtf')
+    return;
+ }
+ if (correo === '' || clave === '' || clave2 === ''){
+   console.log('rellenar');
+   return;
+ }
+ if (clave !== clave2){
+    console.log('error claves');
+    return;
+ }
+console.log('agregamos usuario')
+}
+
+return (
     <>
     <Helmet>
       <title>Crear Cuenta</title>
@@ -14,22 +55,30 @@ const Registro = () => {
       <button >Iniciar Sesion</button>
       </Link>
     </div>
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
       type='email'
       name='email'
       placeholder='Correo Electronico'
+      value={correo}
+      onChange={handleChange}
       />
         <input
       type='password'
       name='password'
       placeholder='Contraseña'
+      value={clave}
+      onChange={handleChange}
+
       />
          <input
       type='password'
       name='password2'
       placeholder='Repetir Contraseña'
-      />
+      value={clave2}
+      onChange={handleChange}
+
+      /> 
       <button type='submit'>Crear Cuenta</button>
     </form>
     </>
--- src/firebase.js
+++ src/firebase.js
@@ -2,29 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth";
 
-const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-
 
 const firebaseConfig = {
   apiKey: "AIzaSyDICwPS6_bsZSA8QinACdt9nJNDzGgAOKE",
   authDomain: "lista-gastos-app.firebaseapp.com",
   projectId: "lista-gastos-app",
   storageBucket: "lista-gastos-app.appspot.com",
-  messagingSenderId: "510113046726",
+  messagingSenderId: "510113046726", 
   appId: "1:510113046726:web:1bca2ef011a3869dcfb7c7"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
+export const auth = getAuth();
