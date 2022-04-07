a38e5d85f6be8a9587b392eec2b3f70ce40a28da
unknown <lamica.olivares@gmail.com>
2022-04-07 10:44:55 -0400

    categorias array- estados select

--- src/component/FormularioGasto.jsx
+++ src/component/FormularioGasto.jsx
@@ -1,46 +1,53 @@
-import {useState} from 'react'
-import {ReactComponent as Add} from '../img/add.svg'
+import { useState } from 'react'
+import { ReactComponent as Add } from '../img/add.svg'
+import SelectCategorias from './SelectCategorias';
 
 const FormularioGasto = () => {
-const[inputDescripcion, cambiarInputDescpcion] = useState('');
-const[inputCantidad, cambiarInputCantidad] = useState('');
+  const [inputDescripcion, cambiarInputDescpcion] = useState('');
+  const [inputCantidad, cambiarInputCantidad] = useState('');
+  const [categoria, cambiarCategoria] = useState('ahorro');
 
-const handleChange = (e) => {
-    if(e.target.name === 'descripcion'){
-        cambiarInputDescpcion(e.target.value);
-    } else  if(e.target.name === 'cantidad'){
-        cambiarInputCantidad(e.target.value.replace(/[^0-9.]/g, ''));
+  const handleChange = (e) => {
+    if (e.target.name === 'descripcion') {
+      cambiarInputDescpcion(e.target.value);
+    } else if (e.target.name === 'cantidad') {
+      cambiarInputCantidad(e.target.value.replace(/[^0-9.]/g, ''));
     }
-}
+  }
 
   return (
-    
+
     <form>
-        <p>
-        <span>Filtros</span>
-        <span>DayPicker</span>
-        </p>
-        <div>
-            <input 
-            type="text"
-            name='descripcion'
-            id='descripcion'
-            placeholder='Descripcion Gasto'
-            value={inputDescripcion}
-            onChange={handleChange}
-             />
-           <input 
-            type="text"
-            name='cantidad'
-            id='cantidad'
-            placeholder='$0.000'
-            value={inputCantidad}
-            onChange={handleChange}
-             $ />
-        </div>
-        <div>
-        <button type='submit'>Agregar Gasto <Add/></button>
-        </div>
+
+      <span>
+        <SelectCategorias
+          categoria={categoria}
+          cambiarCategoria={cambiarCategoria}
+        />
+      </span>
+      <span>DayPicker</span>
+
+      <div>
+        <input
+          type="text"
+          name='descripcion'
+          id='descripcion'
+          placeholder='Descripcion Gasto'
+          value={inputDescripcion}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name='cantidad'
+          id='cantidad'
+          placeholder='$0.000'
+          value={inputCantidad}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <button type='submit'>Agregar Gasto <Add /></button>
+      </div>
     </form>
   )
 }
---
+++ src/component/SelectCategorias.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import{ReactComponent as Down} from '../img/down.svg'
+
+const SelectCategorias = ({categoria, cambiarCategoria}) => {
+    const [mostrarSelect , cambiarMostrarSelect] = useState(false);
+    
+    const categorias = [
+        {id: 'comida', texto: 'Comida'},
+        {id: 'cuentas', texto: 'Cuentas'},
+        {id: 'hogar', texto: 'Hogar'},
+        {id: 'transporte', texto: 'Transporte'},
+        {id: 'ropa', texto: 'Ropa'},
+        {id: 'salud', texto: 'Salud'},
+        {id: 'ahorro', texto: 'Ahorro'},
+        {id: 'diversion', texto: 'Diversion'},
+    ]
+
+const handleClick = (e) =>{
+    cambiarCategoria(e.currentTarget.dataset.valor);
+}
+
+  return (
+      <div onClick={() => cambiarMostrarSelect(!mostrarSelect)}>
+             <div>{categoria} <Down/></div>
+             {mostrarSelect &&
+             <div>
+              {categorias.map((categoria) => {
+                 return <div 
+                 key={categoria.id}
+                 data-valor={categoria.texto}
+                 onClick={handleClick}
+                 >
+                     {categoria.id}
+                     </div>
+              })}
+             </div>
+             }
+      </div>
+ 
+  
+  )
+}
+
+export default SelectCategorias
