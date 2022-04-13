207e26715f33250f88ab2f2c598be9bc59dbb533
unknown <lamica.olivares@gmail.com>
2022-04-13 15:45:35 -0400

    hook conectar a db

--- src/component/ListaGastos.jsx
+++ src/component/ListaGastos.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom'
+import UseObtenerGasto from '../hooks/UseObtenerGasto'
+import { useState } from 'react'
 import  { ReactComponent as Undo} from '../img/undo.svg'
 import BarraTotalGastado from './BarraTotalGastado'
 
 
 
 const ListaGastos = () => {
-
+const [gastos] = UseObtenerGasto();
+console.log(gastos);
  
 return (
     <>
---
+++ src/hooks/UseObtenerGasto.jsx
@@ -0,0 +1,43 @@
+import {useState, useEffect} from 'react'
+import {db} from '../firebase'
+import {useAuth} from '../Context';
+import { collection, onSnapshot, query, orderBy, where, limit } from 'firebase/firestore'
+
+const UseObtenerGasto = () => {
+  const {usuario} = useAuth();
+    const [gastos, cambiarGastos] = useState([]);
+    const[ultimoGasto, cambiarUltimoGasto] = useState(null);
+    const[hayMasPorCargar, cambiarHayMasPorCargar] = useState(false);
+ 
+    const obtenerMasGastos = () => {
+
+    }
+
+    useEffect(()=> {
+ 
+      const consulta = query(
+    collection(db, 'gastos'),
+    where('uidUsuario', '==', usuario.uid),
+    orderBy('fecha', 'desc'),
+    limit(10)
+
+  );
+
+  const unsuscribe = onSnapshot(consulta, (snapshot) => {
+     if(snapshot.docs.length > 0){
+       cambiarUltimoGasto(snapshot.docs[snapshot.docs.length -1 ]);
+       cambiarHayMasPorCargar(true);
+     } else {
+       cambiarHayMasPorCargar(false);
+     }
+     cambiarGastos(snapshot.docs.map((gasto)=> {
+       return{...gasto.data(), id: gasto.id}
+     }));
+  })
+
+return unsuscribe;
+    }, [usuario]);
+    return[gastos, obtenerMasGastos, hayMasPorCargar];
+}
+
+export default UseObtenerGasto;
