69e3c615be897138f7571a04c43270b3b44beb8c
unknown <lamica.olivares@gmail.com>
2022-04-15 11:34:31 -0400

    ordenar gastos por fecha

--- src/component/ListaGastos.jsx
+++ src/component/ListaGastos.jsx
@@ -1,59 +1,84 @@
 import { Link } from 'react-router-dom'
 import useObtenerGasto from '../hooks/useObtenerGasto'
-import  { ReactComponent as Undo} from '../img/undo.svg'
+import { ReactComponent as Undo } from '../img/undo.svg'
 import BarraTotalGastado from './BarraTotalGastado'
 import IconosCategoria from '../element/IconosCategoria'
 import conversorMoneda from '../funciones/ConversorMoneda'
-import  { ReactComponent as Trash} from '../img/trash.svg'
-import  { ReactComponent as Edit} from '../img/edit.svg'
-import  { ReactComponent as Add} from '../img/add.svg'
+import { ReactComponent as Trash } from '../img/trash.svg'
+import { ReactComponent as Edit } from '../img/edit.svg'
+import { ReactComponent as Add } from '../img/add.svg'
+import { format, fromUnixTime } from 'date-fns';
+import { es } from 'date-fns/locale';
 
 
 const ListaGastos = () => {
-const [gastos] = useObtenerGasto();
-console.log(gastos);
- 
-return (
+  const [gastos] = useObtenerGasto();
+  console.log(gastos);
+  const formatearFecha = (fecha) => {
+    return format(fromUnixTime(fecha), "dd 'de' MMMM 'de' yyyy", { locale: es });
+  }
+
+  const fechaIgual = (gastos, index, gasto) => {
+    if (index !== 0) {
+      const fechaActual = formatearFecha(gasto.fecha);
+      const fechaAnterior = formatearFecha(gastos[index - 1].fecha);
+     
+      if (fechaActual === fechaAnterior) {
+        return true;
+
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return (
     <>
-    <div>ListaGastos</div>
-    <Link to="/">
- <button><Undo/></button>
-    </Link>
-   <div>
-     {gastos.map((gasto) => {
-       return(
-       <div key={gasto.id}>
-         <li>
-         <IconosCategoria id={gasto.categoria}/>
-             {gasto.categoria}
-             </li>
-             <li>
-               {gasto.descripcion}
-             </li>
-             <li>
-               {conversorMoneda(gasto.cantidad)}
-             </li>
-             <div>
-             <Link to={`/Editar/${gasto.id}`}>
-               <button ><Edit/></button>
-               </Link>
-               <button><Trash/></button>
-             </div>
+      <div>ListaGastos</div>
+      <Link to="/">
+        <button><Undo /></button>
+      </Link>
+      <div>
+        {gastos.map((gasto, index) => {
+          return (
+            <div key={gasto.id}>
+              {!fechaIgual(gastos, index, gasto) &&
+                <span >
+                  {formatearFecha(gasto.fecha)}
+                </span>
+              }
+              <div key={gasto.id}>
+                <li>
+                  <IconosCategoria id={gasto.categoria} />
+                  {gasto.categoria}
+                </li>
+                <li>
+                  {gasto.descripcion}
+                </li>
+                <li>
+                  {conversorMoneda(gasto.cantidad)}
+                </li>
+                <div>
+                  <Link to={`/Editar/${gasto.id}`}>
+                    <button ><Edit /></button>
+                  </Link>
+                  <button><Trash /></button>
+                </div>
+              </div>
             </div>
-       
-       ); 
-     })}
-   <div><button>Cargar Más</button></div>
-   {gastos.length === 0 && 
-   <span>
-     <p>No Has Agregado Gastos</p>
-     <Link to="/">
-     <button><Add/></button>
-     </Link>
-   </span>
-   }
-   </div>
-    <BarraTotalGastado/>
+          );
+        })}
+        <div><button>Cargar Más</button></div>
+        {gastos.length === 0 &&
+          <span>
+            <p>No Has Agregado Gastos</p>
+            <Link to="/">
+              <button><Add /></button>
+            </Link>
+          </span>
+        }
+      </div>
+      <BarraTotalGastado />
     </>
   )
 }
