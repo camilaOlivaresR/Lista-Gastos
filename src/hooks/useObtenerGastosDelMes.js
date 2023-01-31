a8890c598f5234430225a76339822e0382522b4d
camOR <lamica.olivares@gmail.com>
2023-01-31 18:25:41 -0300

    obtener gasto del mes

---
+++ src/hooks/useObtenerGastosDelMes.js
@@ -0,0 +1,49 @@
+import { useState , useEffect} from "react";
+import { useAuth } from "../Context";
+import { db } from "../firebase";
+import { startOfMonth, endOfMonth, getUnixTime } from "date-fns";
+import { collection, onSnapshot, query, orderBy, query, where, DocumentSnapshot } from "firebase/firestore";
+
+const useObtenerGastoDelMes = () => {
+    const [gastos, establecerGastos] = useState([]);
+    const {usuario} = useAuth();
+
+
+
+useEffect(() => {
+
+    const inicioDeMes = getUnixTime(startOfMonth(new Date()));
+    const finDeMes = getUnixTime(endOfMonth(new Date()));
+
+//   console.log(inicioDeMes, finDeMes);
+
+    if (usuario) {
+        const consulta = query(
+            collection(db, 'gastos'),
+            orderBy('fecha', 'desc'),
+            where('fecha', '>=', inicioDeMes),
+            where('fecha', '<=', finDeMes),
+            where('uidUsuario', '==', usuario.uid)
+        );
+
+        const unsuscribe = onSnapshot(consulta, (snapshot) => {
+            establecerGastos(snapshot.docs.map((documento) => {
+                return {...documento.data(), id: documento.id}
+
+            }))
+          
+        }, (error) => {console.log(error)});
+        return unsuscribe;
+    }
+
+}, [usuario])
+
+
+
+
+
+
+    
+    return [gastos];
+}
+export default useObtenerGastoDelMes;
